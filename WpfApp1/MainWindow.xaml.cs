5e5fa317676709c177149d9f34aee8e995977c6f
Gorgy <egorsasim@mail.ru>
2022-04-17 11:48:31 +0300

    first commit

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using OOPLab2.Figures;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace OOPLab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BaseFigure f;
+        private PathGeometry pathGeometry;
+        private PathFigureCollection fCollection;
+        private Figures.Point firstMousePos;
+        private Figures.Point lastMousePos;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            pathGeometry = new PathGeometry();
+            fCollection = new PathFigureCollection();
+            myPath.Stroke = Brushes.Red;
+            myPath.StrokeThickness = 3;
+        }
+
+        private void myPath_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            System.Windows.Point currentPosition = e.GetPosition(myPath);
+            firstMousePos = new Figures.Point((int)currentPosition.X, (int)currentPosition.Y);
+        }
+
+        private void myPath_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            System.Windows.Point endPosition = e.GetPosition(myPath);
+            lastMousePos = new Figures.Point((int)endPosition.X, (int)endPosition.Y);
+            FigureCollection figuresCollection = new FigureCollection(firstMousePos, lastMousePos);
+            f = figuresCollection.GetFigure(cbFigures.SelectedIndex);
+            List<PathFigure> pathFigureList = new List<PathFigure>(f.drawMethod.Draw(f.GetDots()));
+            foreach (PathFigure f in pathFigureList)
+                fCollection.Add(f);
+            pathGeometry.Figures = fCollection;
+            myPath.Data = pathGeometry;
+        }
+    }
+}
